cb7de763189d6ea2bd9ae476f4ec56d8628ba228
Esmith-Sdev <esmith897@ivytech.edu>
2026-03-20 12:43:51 -0400

    Fixing for deployment

--- fishquest/backend/app.js
+++ fishquest/backend/app.js
@@ -17,7 +17,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://esmith-sdev.github.io",
+  "https://fishquest-frontend.onrender.com",
   "https://fishquest.onrender.com",
 ];
 
