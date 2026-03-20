ce4a8dee352e2382d42337f69e3d64ea63e793d4
Esmith-Sdev <esmith897@ivytech.edu>
2026-03-20 13:00:46 -0400

    Fixing file casing

--- fishquest/frontend/src/components/TipsBox.jsx
+++ fishquest/frontend/src/components/TipsBox.jsx
@@ -1,6 +1,6 @@
 import { Container, Image, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import RigIcon from "../assets/img/Reels/Baitcaster.png";
+import RigIcon from "../assets/img/Reels/baitcaster.png";
 export default function TipsBox() {
   return (
     <Container className="pt-3">
